e346135240de2907fe556e1e9032da2ff5beeda9
Zachary <zacarlin@gmail.com>
2020-09-26 22:43:12 -0400

    Initial Commit

---
+++ scale/scaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  scaleApp.swift
+//  scale
+//
+//  Created by Zachary Carlin on 9/26/20.
+//
+
+import SwiftUI
+
+@main
+struct scaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
